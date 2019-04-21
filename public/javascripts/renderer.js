f3d3623a84362fd3a370af77293fe79372cda477
Luka <com.lukas@gmail.com>
2019-04-21 11:10:33 +0200

    Added raytracing workers

--- public/javascripts/renderer.js
+++ public/javascripts/renderer.js
@@ -1,29 +1,45 @@
 var mouseX, mouseY;
-function onDocumentMouseMove( event ) 
+function onDocumentMouseMove(event) 
 {
 	mouseX = event.clientX;
 	mouseY = event.clientY;
 }
 
+
 var loader = new THREE.GLTFLoader();
-var scene = new THREE.Scene();
+var scene;
+var renderer;
+var camera;
+var controls;
 
-var camera = new THREE.PerspectiveCamera(45, 1920 / 1080, 1, 20000 );
-camera.position.x = -5;
-camera.position.y = 1;
+var hash = location.hash ? location.hash.substring( 1 ) : '3';
 
-var renderer = new THREE.WebGLRenderer();
-renderer.domElement.id = "rendering-canvas";
-renderer.setClearColor('#f4f4f4');
+var WORKERS = + hash || navigator.hardwareConcurrency || 3;
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement); 
-	
-var ambientLight = new THREE.AmbientLight( 0xcccccc );
-scene.add( ambientLight );
+document.body.addEventListener("wheel", wheelCallback);
 
-renderer.setSize(1920, 1080);
-document.body.appendChild(renderer.domElement);
+direction = 'x';
+function wheelCallback() 
+{
+	const delta = Math.sign(event.deltaY);
 
+	var value = delta > 0 ? 0.5 : -0.5;
+
+	if (direction == 'x')
+	{
+		camera.position.x += value;
+	}
+	else if (direction == 'y')
+	{
+		camera.position.y += value;
+	}
+	else if (direction == 'z')
+	{
+		camera.position.z += value;
+	}
+
+	RENDERER.onRenderFrame();
+}
 
 
 var RENDERER =
@@ -57,11 +73,22 @@ var RENDERER =
 	 * Flag indicating if screenshot should be captured on next frame render.
 	 */
 	getScreenshot: false,
-
 	
 	init: function()
 	{
-		
+
+		scene = new THREE.Scene();
+		scene.background = new THREE.Color( 0xf0f0f0 );
+
+		camera = new THREE.PerspectiveCamera(45, 1920 / 1080, 1, 20000);
+		/*camera.position.x = -10.6;
+		camera.position.y = 12.6;
+		camera.position.z = -13.6;*/
+		camera.position.x = 0;
+		camera.position.y = 5.5;
+		camera.position.z = 32;
+		//scene.add(camera);
+	
 
 
 		RENDERER.io = io(RENDERER.hostingUrl, { reconnect: true });
@@ -72,9 +99,6 @@ var RENDERER =
 		RENDERER.io.on('connect', RENDERER.onServerConnected);
 
 		document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-		// start rendering
-		RENDERER.onRenderFrame();
 	},
 
 
@@ -108,9 +132,64 @@ var RENDERER =
 		};
 		var onLoadFinished = function(gltf) 
 		{
-			console.error('[glTF loader] Scene finished loading');
+			console.log('[glTF loader] Scene finished loading');
+			
+			
 
 			scene.add(gltf.scene);
+			//scene = gltf.scene;
+
+			scene.background = new THREE.Color( 0xf0f0f0 );
+			var ambientLight = new THREE.AmbientLight(0xcccccc);
+		scene.add(ambientLight);
+
+		/*var intensity = 7000000000;
+
+				var light = new THREE.PointLight( 0xffaa55, intensity );
+				light.position.set( - 200, 100, 100 );
+				light.physicalAttenuation = true;
+				scene.add( light );
+
+				var light = new THREE.PointLight( 0x55aaff, intensity );
+				light.position.set( 200, 100, 100 );
+				light.physicalAttenuation = true;
+				scene.add( light );
+
+				var light = new THREE.PointLight( 0xffffff, intensity * 1.5 );
+				light.position.set( 0, 0, 300 );
+				light.physicalAttenuation = true;
+				scene.add( light );*/
+			/*for (var i=0; i<gltf.scene.children.length; i++)
+			{
+				var current = gltf.scene.children[i].material;
+
+				if (!current)
+				{
+					continue;
+				}
+
+				current.color= 0x666666;
+				current.specular= 0x111111;
+				current.shininess= 100;
+				current.vertexColors= THREE.NoColors;
+				current.flatShading= true;
+			}*/
+
+			/*renderer = new THREE.WebGLRenderer();*/
+			renderer = new THREE.RaytracingRenderer( {
+				workers: WORKERS,
+				workerPath: 'javascripts/threejs/RaytracingWorker.js',
+				randomize: true,
+				blockSize: 64
+			} );
+
+			renderer.domElement.id = "rendering-canvas";
+			renderer.setSize(1920, 1080);
+			renderer.clear();
+			renderer.setClearColor('#f4f4f4');
+			//controls = new THREE.OrbitControls(camera, renderer.domElement); 
+			
+		document.body.appendChild(renderer.domElement);
 
 			gltf.animations; // Array<THREE.AnimationClip>
 			gltf.scene; // THREE.Scene
@@ -118,10 +197,14 @@ var RENDERER =
 			gltf.cameras; // Array<THREE.Camera>
 			gltf.asset; // Object
 			
-			// refresh camera (if not then color of the pixels is incorrect)
-			controls.update();
+// start rendering
+RENDERER.onRenderFrame();
 
-			RENDERER.getScreenshot = true;				
+			// refresh camera (if not then color of the pixels is incorrect)
+			//controls.update();
+
+
+			//RENDERER.getScreenshot = true;	
 		};
 
 		loader.load(filepath, onLoadFinished, onLoadingProgress, onLoadingError);
@@ -198,7 +281,7 @@ var RENDERER =
 	onRenderFrame: function()
 	{
 		// will start loop for this function
-		requestAnimationFrame(RENDERER.onRenderFrame);
+		//requestAnimationFrame(RENDERER.onRenderFrame);
 
 		// render current frame
 		renderer.render(scene, camera);
@@ -212,7 +295,12 @@ var RENDERER =
 		}		
 
 		// update camera
-		controls.update();
+		if (controls)
+		{
+			controls.update();
+			console.log(camera.position.x + ',' + camera.position.y + ', ' + camera.position.z);
+		}
+		
 	},
 
 	onGetLayout: function(data)
@@ -240,6 +328,7 @@ var RENDERER =
 		}				
 
 		// Load a glTF resource
+		//RENDERER.startLoadingGltfModel('files/castle/Sponza.gltf');
 		RENDERER.startLoadingGltfModel('files/Scene.gltf');
 
 		RENDERER.requestCellAsync();
