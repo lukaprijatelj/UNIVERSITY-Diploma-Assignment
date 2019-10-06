77ef04b53818f24ea8d6292a5cead989a0c1c295
Luka <com.lukas@gmail.com>
2019-10-06 12:43:43 +0200

    Stylesheets updated

--- public/scripts/options.js
+++ public/scripts/options.js
@@ -6,15 +6,16 @@ var options =
 	//SCENE_FILEPATH: 'scenes/MetalRoughSpheres/Scene.gltf',
 	SCENE_FILEPATH: 'scenes/boomBox/Scene.gltf',
 
-	SKY_CUBE_FILEPATH: 'images/sor_lake1/',
+	//SKY_CUBE_FILEPATH: 'images/sor_lake1/',
 	//SKY_CUBE_FILEPATH: 'images/skycube_2/',
+	SKY_CUBE_FILEPATH: 'images/black/',
 
 
 	RESOLUTION_FACTOR: 1,
 	CANVAS_WIDTH: 1280,
 	CANVAS_HEIGHT: 720,
-	BLOCK_WIDTH: 20,
-	BLOCK_HEIGHT: 20,
+	BLOCK_WIDTH: 25,
+	BLOCK_HEIGHT: 25,
 	NUM_OF_BLOCKS_IN_CHUNK: 10,
 
 	CAMERA: null,
