2c90bc6058bfe2b0937ee26ebda196b29b72c9b9
lukaprijatelj <com.lukas@gmail.com>
2020-02-08 16:08:44 +0100

    Added redirection from index page to client page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) 
+{
+	// res.render('index', { title: 'Express' });
+	  
+	// redirect to CLIENT page
+	res.redirect('/client');
 });
 
 module.exports = router;
